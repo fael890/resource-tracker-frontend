3bc3e76aa6e3fc1b5b5cbfbfcf220f92a43c84de
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-03-04 17:28:54 -0300

    feat: add header component and remove example files

---
+++ src/components/header/header.tsx
@@ -0,0 +1,10 @@
+import './header.css';
+
+export function Header(){
+    return (
+        <header id="container">
+            <a href="index.html">PROCESS MONITORING</a>
+            <a href="index.html">PROCESS RANKING</a>
+        </header>
+    )
+}
