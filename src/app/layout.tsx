6efd5c5da2718c2634e855cc095eba08ee154435
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-03-20 15:57:08 -0300

    refactor: remove unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 export const metadata = {
   title: 'Next.js',
